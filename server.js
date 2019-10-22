7fab99087d098459274fcfd6eb4dd26184265bc7
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2019-10-22 22:32:01 +0300

    add bootcamps controllers and route

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
+
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
