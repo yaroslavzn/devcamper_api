172ad90cdadb195798407c9829f829f2aeadf2bf
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2019-10-24 22:46:19 +0300

    add mongoose package, create db connection, add colors package

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
+
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to the DB
+connectDB();
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
@@ -19,8 +25,14 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(
     `Server started in ${process.env.NODE_ENV} mode, on the ${PORT} port.`
+      .yellow.bold
   );
+});
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  server.close(() => process.exit(1));
 });
